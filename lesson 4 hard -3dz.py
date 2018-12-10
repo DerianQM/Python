068fab96bc7efb662a1694bcd2d3f2bfdc43f2e3
DerianQM <44587394+DerianQM@users.noreply.github.com>
2018-12-10 08:46:02 +0300

    Задание 3 уровня hard урока 4
    не могу понять причину зацикливания

---
+++ lesson 4 hard -3dz.py
@@ -0,0 +1,78 @@
+mass = [[3, 1], [6, 2], [4,3], [2, 4], [8, 5], [5, 6], [7, 7], [1, 8]]# верный ряд, поменять любую цифру, чтоюы получить нет
+# разблокировать, чтобы не вводить
+#print(mass)
+
+def horizontal(mass, row):
+    tempmass = mass.copy()
+    tempmass.remove(row)
+    for ch  in tempmass:
+        if row[0] == ch[0]:
+            print("NO")
+            exit()
+        if row[1] == ch[1]:
+            print("NO")
+            exit()
+def diagonal(mass,row):
+    tempmass = mass.copy()
+    tempmass.remove(row)
+    for ch in tempmass:
+        i=0
+        j=0   
+        while (8-(ch[0]+i))!=0 or (8-(ch[1]+j))!=0:
+            if row[0] == ch[0]+i:
+                print("NO")
+                exit()
+            if row[1] == ch[1]+j:
+                print("NO")
+                exit()
+            i+=1
+            j+=1
+           
+
+    for ch in tempmass:
+        i = 0
+        j = 0
+        while (8 - (ch[0] - i)) != 8 or (8 - (ch[1] - j)) != 8:
+            if row[0] == ch[0]-i:
+                print("NO")
+                exit()
+            if row[1] == ch[1]-j:
+                print("NO")
+                exit()
+            i+=1
+            j+=1
+    
+    for ch in tempmass:
+        i = 0
+        j = 0
+        while (8 - (ch[0] + i)) != 0 or (8 - (ch[1] - j)) != 8:
+            if row[0] == ch[0]+i:
+                print("NO")
+                exit()
+            if row[1] == ch[1]-j:
+                print("NO")
+                exit()
+            i += 1
+            j += 1
+    
+    for ch in tempmass:
+        i = 0
+        j = 0
+        while (8 - (ch[0] - i)) != 8 or (8 - (ch[1] + j)) != 0:
+            if row[0] == ch[0]-i:
+                print("NO")
+                exit()
+            if row[1] == ch[1]+j:
+                print("NO")
+                exit()
+            i += 1
+            j += 1
+
+
+
+
+
+for row in mass:
+     horizontal(mass, row) # проверка горизонталей и вертикалей
+     diagonal(mass, row) #проверка диагоналей
+print("YES")
